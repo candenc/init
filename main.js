86dd983a2c84aa35741c9408a2bf0aaaedfc69cc
Cande Campagnolo <candecampagnolo@outlook.es>
2022-05-05 13:34:08 -0300

    Agregar

--- main.js
+++ main.js
@@ -1,7 +1,49 @@
 let menuB=document.getElementById("menuB").addEventListener('click', ()=>{
     document.querySelector('.nav-menu').classList.toggle('show');
 })
+let btn1=document.getElementById("btn1").addEventListener('click', ()=>{
+    console.log("Haz clickeado");
+    
+})
+let btn2=document.getElementById("btn2").addEventListener('click', ()=>{
+    console.log("Haz clickeado");
+    
+})
 
-scrollReveal().reveal('.showcase');
-scrollReveal().reveal('.notice', {delay:500});
-scrollReveal().reveal('.banner',  {delay:500});
+const baseDatos=window.localStorage;
+
+const nombre=document.getElementById("name");
+const edad=document.getElementById("numero");
+const direccion=document.getElementById("direccion"); 
+
+const guadarContacto = (baseDatos,contacto) => {
+    baseDatos.setItem(contacto.id, JSON.stringify(contacto));
+
+} 
+
+let btnAg=document.getElementById("btnAg").addEventListener('click', (e)=>{
+    console.log("Haz clickeado");
+    e.preventDefault(e);
+    let contacto= {
+        nombre:nombre.value,
+        numero:edad.value,
+        direccion:direccion.value,
+    }
+    guadarContacto(baseDatos,contacto);
+})
+
+
+
+const cargarContactos=(baseDatos)=>{
+    let claves= Object.keys(baseDatos);
+    console.log(claves);
+    for (clave of claves){
+        console.log(clave);
+        let contacto=JSON.parse(baseDatos.getItem(clave));
+        console.log(contacto);
+    }
+}
+
+
+cargarContactos(baseDatos);
+
